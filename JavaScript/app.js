aa947d3611ba34d2ee25eda65ecd8d353491b267
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-04-05 09:21:27 +0000

    adding new branch todo-js2

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,32 +1,32 @@
 "use strict"
-// let userName = prompt("What is your name?");
-// let gender = prompt("Please enter your gender:","male or female");
+let userName = prompt("What is your name?");
+let gender = prompt("Please enter your gender:","male or female");
 
-// if (gender != "male" && gender != "female") {
-//     gender = prompt("Please enter your gender:", "male or female");
-// }
+if (gender != "male" && gender != "female") {
+    gender = prompt("Please enter your gender:", "male or female");
+}
 
-// let nickName = "";
-// if (gender == "male") {
-//     nickName = "Mr";
-// }
-// else if (gender == "female") {
-//     nickName = "Ms";
-// }
-
-
-// let age = parseInt(prompt("Please enter your age", "Your age"));
-// if (age <= 0) {
-//     alert("Please enter your correct age");
-//     age = parseInt(prompt("Please enter your age", "Your age"));
-// }
+let nickName = "";
+if (gender == "male") {
+    nickName = "Mr";
+}
+else if (gender == "female") {
+    nickName = "Ms";
+}
 
 
-// let showWelcoming = confirm("Do you want to show your welcome message?");
-// console.log(showWelcoming)
-// if (showWelcoming === true) {
-// alert ("Hello " + nickName +" "+userName +"\nWelcome to my To-Do List!");
-// }
+let age = parseInt(prompt("Please enter your age", "Your age"));
+if (age <= 0) {
+    alert("Please enter your correct age");
+    age = parseInt(prompt("Please enter your age", "Your age"));
+}
+
+
+let showWelcoming = confirm("Do you want to show your welcome message?");
+console.log(showWelcoming)
+if (showWelcoming === true) {
+alert ("Hello " + nickName +" "+userName +"\nWelcome to my To-Do List!");
+}
 
 const yesOrNoQuestions = (qustion) => {
     let answer = prompt (qustion);
